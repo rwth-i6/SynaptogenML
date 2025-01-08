6cea80b6f3d65afac2731702e770f2b9a4cc3cb2
Nick Rossenbach <rossenbach@i6.informatik.rwth-aachen.de>
2025-01-08 13:33:41 +0100

    update test

--- tests/test_mnist_tiling_linear.py
+++ tests/test_mnist_tiling_linear.py
@@ -1,16 +1,13 @@
-import copy
-
+from mnist_helper import run_training
+import pytest
 import torch
 from torch import nn
-import time
 from torch_memristor.quant_modules import LinearQuant, ActivationQuantizer
-import sys
 
 
 from torch_memristor.memristor_modules import TiledMemristorLinear, DacAdcHardwareSettings
-from mnist_helper import create_mnist_dataloaders
 
-class QuantizedModel2(nn.Module):
+class TilingLinearModel(nn.Module):
 
     def __init__(self):
         super().__init__()
@@ -111,100 +108,7 @@ class QuantizedModel2(nn.Module):
         self.memristor_final.init_from_linear_quant(self.activation_quant_final_in, self.final_linear)
 
 
+@pytest.mark.linear
 def test_linear():
-    """
+    run_training(TilingLinearModel, expected_accuracy=0.5)
 
-    :return:
-    """
-    BATCH_SIZE = 10
-    NUM_EPOCHS = 5
-
-    dataloader_train, dataloader_test = create_mnist_dataloaders(BATCH_SIZE)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("device: %s" % device)
-
-    model = QuantizedModel2()
-    model.to(device=device)
-    optimizer = torch.optim.RAdam(lr=1e-4, params=model.parameters())
-
-    # do a train step
-    for i in range(NUM_EPOCHS):
-        print("\nstart train epoch %i" % i)
-        total_ce = 0
-        total_acc = 0
-        num_examples = 0
-        model.to(device=device)
-        model.train()
-
-        for data in dataloader_train:
-            image, labels = data
-            num_examples += image.shape[0]
-            if num_examples > 2000:
-                # do not train so much on CPU
-                break
-            image = image.to(device=device)
-            labels = labels.to(device=device)
-            logits = model.forward(image)
-            ce = nn.functional.cross_entropy(logits, target=labels, reduction="sum")
-            total_ce += ce.detach().cpu()
-            acc = torch.sum(torch.eq(torch.argmax(logits, dim=-1), labels).int())
-            total_acc += acc.detach().cpu()
-            # print(f"CE: {ce/BATCH_SIZE:.3f}  ACC: {acc/BATCH_SIZE:.3f}")
-            ce.backward()
-
-            optimizer.step()
-            optimizer.zero_grad()
-
-        print(f"train ce: {total_ce / num_examples:.3f} acc: {total_acc / num_examples:.3f}")
-        total_ce = 0
-        total_acc = 0
-        num_examples = 0
-        model.eval()
-        print("\nstart normal-quant evaluation")
-        start = time.time()
-        for data in dataloader_test:
-            start_tmp = time.time()
-            image, labels = data
-            image = image.to(device=device)
-            labels = labels.to(device=device)
-            num_examples += image.shape[0]
-            with torch.no_grad():
-                logits = model.forward(image)
-            ce = nn.functional.cross_entropy(logits, target=labels, reduction="sum")
-            total_ce += ce.detach().cpu()
-            acc = torch.sum(torch.eq(torch.argmax(logits, dim=-1), labels).int())
-            total_acc += acc.detach().cpu()
-            #if num_examples < 100:
-            #    print(time.time() - start_tmp)
-            # print(f"CE: {ce/BATCH_SIZE:.3f}  ACC: {acc/BATCH_SIZE:.3f}")
-        end_float = time.time() - start
-        end_float_avg = end_float / num_examples
-
-        print(f"test ce: {total_ce / num_examples:.6f}  acc: {total_acc / num_examples:.6f} time: {end_float} per sample: {end_float_avg}")
-
-        model.prepare_memristor()
-        model.to(device=device)
-        
-        print("\nstart memristor evaluation")
-        start = time.time()
-        for data in dataloader_test:
-            start_tmp = time.time()
-            image, labels = data
-            image = image.to(device=device)
-            labels = labels.to(device=device)
-            num_examples += image.shape[0]
-            with torch.no_grad():
-                logits = model.forward(image, use_memristor=True)
-            ce = nn.functional.cross_entropy(logits, target=labels, reduction="sum")
-            total_ce += ce.detach().cpu()
-            acc = torch.sum(torch.eq(torch.argmax(logits, dim=-1), labels).int())
-            total_acc += acc.detach().cpu()
-            #if num_examples < 100:
-            #    print(time.time() - start_tmp)
-            # print(f"CE: {ce/BATCH_SIZE:.3f}  ACC: {acc/BATCH_SIZE:.3f}")
-        end_float = time.time() - start
-        end_float_avg = end_float / num_examples
-
-
-        print(f"test memristor ce: {total_ce / num_examples:.6f}  acc: {total_acc / num_examples:.6f} time: {end_float} per sample: {end_float_avg}")
