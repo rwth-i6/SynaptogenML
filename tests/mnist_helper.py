f591545d6572a5f21841bfab25c6a6d801c0ea76
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2025-06-26 14:19:40 +0200

    remove constraint

--- tests/mnist_helper.py
+++ tests/mnist_helper.py
@@ -69,9 +69,9 @@ def run_training(
         for data in dataloader_train:
             image, labels = data
             num_examples += image.shape[0]
-            if device == "cpu" and num_examples > 2000:
-                # do not train so much on CPU
-                break
+            # if device == "cpu" and num_examples > 2000:
+            #     # do not train so much on CPU
+            #     break
             image = image.to(device=device)
             labels = labels.to(device=device)
             logits = model.forward(image)
