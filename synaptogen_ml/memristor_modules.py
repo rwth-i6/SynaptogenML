3df05fa5ce03534383b4fe2534a7b367ab6644df
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2025-07-02 10:19:40 +0200

    security assert

--- synaptogen_ml/memristor_modules.py
+++ synaptogen_ml/memristor_modules.py
@@ -532,6 +532,8 @@ class MemristorConv1d(nn.Module):
         assert groups > 0
         assert in_channels % groups == 0
         self.groups = groups
+        if not groups == in_channels == out_channels:
+            raise NotImplementedError
         assert kernel_size > 0
         self.kernel_size = kernel_size
         if isinstance(padding, int):
