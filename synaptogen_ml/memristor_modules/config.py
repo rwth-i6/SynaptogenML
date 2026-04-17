57022106c095d51e1dade3c60e7b4b882900c054
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2026-04-17 09:41:14 +0200

    ruff

--- synaptogen_ml/memristor_modules/config.py
+++ synaptogen_ml/memristor_modules/config.py
@@ -4,12 +4,13 @@ __all__ = [
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class CycleCorrectionSettings:
     num_cycles: Optional[int]  # how often
     test_input_value: Optional[float]  # what to feed to check if working
     relative_deviation: Optional[float]  # acceptable deviation
-    ideal_programming: bool # whether to assume perfect programming, requires every other value to be None
+    ideal_programming: bool  # whether to assume perfect programming, requires every other value to be None
 
     def __post_init__(self):
         if self.ideal_programming is True:
